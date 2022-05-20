c87b167921f1fb4703830856d4f217812907735e
simmon <rafal_wilczewski@wp.pl>
2022-05-20 12:17:41 +0200

    React router integrated and styles for button-like links fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
 import React, {useState} from 'react';
+import {Route, Routes } from 'react-router-dom';
 import {Header} from "./components/layout/Header";
 import {Map} from "./components/Map/Map";
 import { SearchContext } from './contexts/search.contexts';
+import {AddForm} from "./components/AddForm/AddForm";
+
 
 export const App = () => {
     const [search, setSearch] = useState('');
 
   return (
-      <>
         <SearchContext.Provider value={{search, setSearch}}>
             <Header/>
-            <Map/>
+            <Routes>
+                <Route path="/" element={<Map/>}/>
+                <Route path="/add" element={<AddForm/>}/>
+            </Routes>
         </SearchContext.Provider>
-      </>
-  );
+     );
 };
